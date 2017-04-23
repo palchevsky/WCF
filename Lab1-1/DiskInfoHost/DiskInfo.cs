958ee98d8d4db61fc28fcdfbee099c77c3f108da
palchevsky <palchevsky@outlook.com>
2017-04-23 22:24:19 +0300

    DiskInfo for lab 1-1

---
+++ Lab1-1/DiskInfoHost/DiskInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DiskInfoHost
+{
+    public class DiskInfo : IDiskInfo
+    {
+        List<string> dirsAndFiles = new List<string>();
+
+        public string[] GetDiskInfo(string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+                foreach (var dir in dirs)
+                {
+                    dirsAndFiles.Add("DIRECTORY\t" + Path.GetFileName(dir));
+                }
+                foreach (var file in files)
+                {
+                    dirsAndFiles.Add("FILE\t" + Path.GetFileName(file));
+                }
+                return dirsAndFiles.ToArray();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            return dirsAndFiles.ToArray();
+        }
+    }
+}
---
+++ Lab1-1/DiskInfoHost/IDiskInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace DiskInfoHost
+{
+    [ServiceContract]
+    public interface IDiskInfo
+    {
+        [OperationContract]
+        string[] GetDiskInfo(string path);
+    }
+}
