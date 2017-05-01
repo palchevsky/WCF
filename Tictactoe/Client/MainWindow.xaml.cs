0cf22a643849069af154622bdd6ff59ee1200e58
Andrew Palchevsky <palchevsky@outlook.com>
2017-05-01 13:35:16 +0300

    TicTacToe

--- Tictactoe/Client/MainWindow.xaml.cs
+++ Tictactoe/Client/MainWindow.xaml.cs
@@ -20,10 +20,101 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool turn=true; //true = X turn, false = 0 turn;
+        private int turnsCount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             //NewGame();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if(turn)
+            {
+                button.Content = "X";
+            }
+            else
+            {
+                button.Content = "O";
+            }
+            turn = !turn;
+            turnsCount++;
+            button.IsEnabled = false;
+            CheckWinner();
+        }
+
+        private void CheckWinner()
+        {
+            bool winner = false;
+            //horizontal
+            if (ButtonA1.Content.Equals(ButtonA2.Content) && ButtonA2.Content.Equals(ButtonA3.Content) && !ButtonA1.IsEnabled)
+            {
+                winner = true;
+            }
+            else if (ButtonB1.Content.Equals(ButtonB2.Content) && ButtonB2.Content.Equals(ButtonB3.Content) && !ButtonB1.IsEnabled)
+            {
+                winner = true;
+            }
+            else if (ButtonC1.Content.Equals(ButtonC2.Content) && ButtonC2.Content.Equals(ButtonC3.Content) && !ButtonC1.IsEnabled)
+            {
+                winner = true;
+            }
+            //vertical
+            if (ButtonA1.Content.Equals(ButtonB1.Content) && ButtonB1.Content.Equals(ButtonC1.Content) && !ButtonA1.IsEnabled)
+            {
+                winner = true;
+            }
+            else if (ButtonA2.Content.Equals(ButtonB2.Content) && ButtonB2.Content.Equals(ButtonC2.Content) && !ButtonA2.IsEnabled)
+            {
+                winner = true;
+            }
+            else if (ButtonA3.Content.Equals(ButtonB3.Content) && ButtonB3.Content.Equals(ButtonC3.Content) && !ButtonA3.IsEnabled)
+            {
+                winner = true;
+            }
+            //diagonal
+            if (ButtonA1.Content.Equals(ButtonB2.Content) && ButtonB2.Content.Equals(ButtonC3.Content) && !ButtonA1.IsEnabled)
+            {
+                winner = true;
+            }
+            else if (ButtonA3.Content.Equals(ButtonB2.Content) && ButtonB2.Content.Equals(ButtonC1.Content) && !ButtonA3.IsEnabled)
+            {
+                winner = true;
+            }
+
+            if (winner)
+            {
+                DisableButtons();
+                switch (turn)
+                {
+                    case true:
+                        MessageBox.Show("O won!");
+                        break;
+                    case false:
+                        MessageBox.Show("X won!");
+                        break;
+                }
+            }
+            else
+            {
+                if (turnsCount==9)
+                {
+                    MessageBox.Show("There is no winner!", "End of Game!");
+                }
+            }
+        }
+
+        private void DisableButtons()
+        {
+            IEnumerable<Button> Buttons = Field.Children.OfType<Button>();
+
+            foreach (Button item in Buttons)
+            {
+                item.IsEnabled = false;
+            }
         }
     }
 }
